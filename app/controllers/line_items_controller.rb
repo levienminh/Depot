1f9751062c080e343057f7e2f28b5bba637fb458
levienminh <levienminh2606a@gmail.com>
2013-08-25 09:25:33 +0700

    Add to Dash of Ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -53,12 +53,12 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-          notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url } 
+        format.js { @current_item = @line_item }
         format.json { render json: @line_item,
           status: :created, location: @line_item }
       else
-        format.html { render action: "new" }
+        format.html { render action: "new" }  
         format.json { render json: @line_item.errors,
           status: :unprocessable_entity }
       end
